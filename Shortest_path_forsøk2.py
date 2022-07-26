87887870d6fb246463552d1f841d2557d571fa47
Bon CO2 <paulius.cebatarauskas@sintef.no>
2022-07-26 14:50:03 +0200

    Added error_analysis_multiple_grids

--- Shortest_path_forsøk2.py
+++ Shortest_path_forsøk2.py
@@ -3,7 +3,7 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def define_neighbors(row, df, G):
+def define_neighbors(row, df, G, gridsize):
     """
     Function used to define gridcell neighbors. After neighbors are found the edge from point to neighbor is added to a network. In this network
     edges to horizontal and vertical neighbors have weight 1, while diagonal neighbors have 1.4(approx sqrt(2)). The graph has to be initiated
@@ -24,18 +24,19 @@ def define_neighbors(row, df, G):
     for i, dataf in enumerate([horz, vert, diag]):
         weight = 1
         if i <2:
-            weight=1*5
+            weight=1
         elif i == 2:
-            weight=1.4*5
+            weight=1.4
         else:
             raise Exception("No weight was assigned for neighbors.")
+        weight *= gridsize
         idx2 = dataf['id'].values
         arr = np.zeros(shape=(len(idx2), 3))
         arr[:,0] = idx2
         arr[:,1:] = np.array([idx, weight])
         G.add_weighted_edges_from(arr)
 
-def shortest_path_to_node(filename="Poland/Ze_points_5.0.csv", id=7080):
+def shortest_path_to_node(filename="Poland/Ze_points_5.0.csv", id=7080, gridsize=5):
     """
     Given a dataset(filename) we convert it into a pandas dataframe. Then we define the network through the use of define negihbors.
     Lastly, we compute the shortest distance to a node from every other node.
@@ -43,6 +44,7 @@ def shortest_path_to_node(filename="Poland/Ze_points_5.0.csv", id=7080):
     Input:
         -filename(str): filename of the file with the data. Should be a CSV file.
         -id(int): id of the point we want to travel to.
+        -gridsize(int): size of grid
     
     """
     #Initializing the dataframe
@@ -53,7 +55,7 @@ def shortest_path_to_node(filename="Poland/Ze_points_5.0.csv", id=7080):
     for method in ['road', 'rail', 'water']:
         method_df = df.loc[df[method]] # extract the nodes with method
         G = nx.Graph()
-        method_df.apply(define_neighbors, axis=1, df=method_df, G=G) #Adds edges to the graph with weights
+        method_df.apply(define_neighbors, axis=1, df=method_df, G=G, gridsize = gridsize) #Adds edges to the graph with weights
         distances = nx.single_source_dijkstra_path_length(G, id) # Computes the shortest distance from/to the node with the specified id. Output is a dictionary.
         distances = dict((int(key), value) for key,value in distances.items())
         col_name = 'distance_' + method
@@ -61,7 +63,7 @@ def shortest_path_to_node(filename="Poland/Ze_points_5.0.csv", id=7080):
     df.to_csv('Fil_med_Avstand.csv', index=False)
     #Det er mulig å returnere dataframen og grafene hvis vi ønsker det. Alternativt så kan vi bare lagre det som filer og åpne de fra en annen funsksjon
 
-def error_analysis(our_data_file, QGIS_data_file):
+def error_analysis(our_data_file, QGIS_data_file, method, multiplier):
     our_df = pd.read_csv(our_data_file) #This gives a dataframe automatically
 
     #Playing with QGIS dataframe, till it looks nice.
@@ -73,12 +75,11 @@ def error_analysis(our_data_file, QGIS_data_file):
     Qgis_df = Qgis_df.astype(convert_dict) #Change type of columns as defined in convert_dict
     
     #Finding the relevant parts of the dataframe to analyze
-    Qgis_df = Qgis_df.loc[Qgis_df['road']] #choosing only the rows that has road available
-    our_df = our_df.loc[our_df['road']] # same as above only for another dataframe
+    Qgis_df = Qgis_df.loc[Qgis_df[method]] #choosing only the rows that has road available
+    our_df = our_df.loc[our_df[method]] # same as above only for another dataframe
 
     #creating error dataframe
     error_df = Qgis_df[['id','x','y']] #Storing the id, x and y values frpm QGIS dataframe
-    multiplier = 1.1 #1.1 funker best for 5km road 
     err = np.array(Qgis_df['cost'] - our_df['distance_road']*multiplier) # Hvorfor klager de så jævlig på denne linja
     error_df.insert(3, 'abs_error', err, True) #Finally
 
@@ -91,12 +92,11 @@ def error_analysis(our_data_file, QGIS_data_file):
     mean_rel_error = np.mean(np.abs(rel_error))
     rmse = np.sqrt(np.sum(abs_error**2)/np.size(abs_error)) #root mean square error
     print(mean_rel_error, rmse)
-    #Hvis jeg vil bruke make contour så trenger jeg indeksene. Hvordan kan jeg gjøre det da?
-    return error_df
+    return error_df, mean_rel_error, rmse
 
 
 def make_contour():
-    error_df = error_analysis('Fil_med_Avstand.csv', 'Poland/distance_road_5.csv')
+    error_df, mean_rel_error, rmse = error_analysis('Fil_med_Avstand.csv', 'Poland/distance_road_5.csv') #use only the error_df
     #How do we remove the rows that are np.nan?
     error_df = error_df.loc[np.isfinite(error_df['rel_error'])] #removing nan from rel_error
     error_df = error_df.loc[np.isfinite(error_df['abs_error'])] #doing the same thing for abs_error, but I don't think that is necessary. If nan in abs_error then that should propagate to rel_error, as it uses abs_error
@@ -116,10 +116,28 @@ def make_contour():
     #plt.savefig(plot_name)
     plt.clf() #Clear the old figure
 
+def error_analysis_multiple_grids():
+    gridsizes = [i for i in range(0,51, 5) if i!= 0] # [5, 10, ..., 50]
+    id_poland = {50:67, 45:86, 40:114, 35:146,30:192,25:278,20:437,15:760,10:1742, 5:7080}
+    multiplier = 1.1
+    stats = np.zeros(shape=(len(gridsizes), 4))
+    stats[:,0] = gridsizes
+    print(stats)
+    for i, gridsize in enumerate(gridsizes):
+        filename = 'Poland/Ze_points_{:.1f}.csv'.format(gridsize)
+        shortest_path_to_node(filename, id_poland[gridsize], gridsize) # Det lages en fil som heter "Fil_med_Avstand.csv"
+        for j, method in enumerate(['road', 'rail', 'water']):
+            distance_qgis_fn = "Poland/distance_{}_{}.csv".format(method, gridsize)
+            error_df, mean_rel_error, rmse = error_analysis('Fil_med_Avstand.csv',distance_qgis_fn,method,multiplier)
+            stats[i,j+1] = mean_rel_error
+            print(stats)
 
 
 
 
 
-shortest_path_to_node()
-make_contour()
+
+
+#shortest_path_to_node()
+#make_contour()
+error_analysis_multiple_grids()
